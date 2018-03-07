6524f153721064d5c9cc74767c457380aa544e0c
Alan Luiz <alanlviana@hotmail.com>
2018-03-07 16:14:19 -0300

    Provisioriamente foram adicionados arquivos finais do angular

--- src/app/pedido/pages/pedido-form/pedido-form.component.ts
+++ src/app/pedido/pages/pedido-form/pedido-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Http  } from '@angular/http';
 import { forEach } from '@angular/router/src/utils/collection';
+import { Observable } from 'rxjs/Observable';
+import { Response } from '@angular/http/src/static_response';
 
 @Component({
   selector: 'app-pedido-form',
@@ -11,6 +13,14 @@ export class PedidoFormComponent implements OnInit {
 
   nome: string = "";
   localEntrega: string = "";
+  card: any = {
+    identification: "41006420835",
+    name: "Alan Luiz Viana",
+    number: "4235647728025682",
+    expirationMonth: "12",
+    expirationYear: "2015",
+    cvv: "123"
+  }
   itensPedido : any = [];
 
   baseUrl:string = "";
@@ -34,8 +44,11 @@ export class PedidoFormComponent implements OnInit {
                       .reduce((sum,current) => sum + current,0);
   }
 
-  finalizarPedido(){
+  async finalizarPedido(){
     
+    // Valida cartão
+    var cardToken = await this.validaCartao(this.card);
+
     var itensParaEnvio = this.itensPedido.filter(item => item.quantidade > 0);
     
     var checkout : any = {
@@ -54,5 +67,18 @@ export class PedidoFormComponent implements OnInit {
     });
   }
 
-
+  async validaCartao(card) {
+    var response = await this._http.post("https://api.mercadopago.com/v1/card_tokens?public_key=TEST-17b12ef3-9d1d-4e98-b87d-d73f350d6cef",{
+      "expirationYear": card.expirationYear,
+      "expirationMonth": card.expirationMonth,
+      "cardholder": {
+        "identification":{
+          "number":card.identification,
+          "type":"CPF"
+        }
+      }
+    }).toPromise();
+    console.log(response);
+    return response;
+  }
 }
